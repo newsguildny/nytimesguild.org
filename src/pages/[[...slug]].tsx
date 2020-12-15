09e37981365e900aa5a875b3f996b7c1b759b4c4
Angelique De Castro <angeliqueajdecastro@gmail.com>
2020-12-15 00:45:27 -0500

    remove old styling

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -32,36 +32,6 @@ const Page = ({ source, title }: Props) => {
       <main>
         <div>{content}</div>
       </main>
-      <style jsx>{`
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.75rem;
-          font-weight: 400;
-          line-height: 1.25;
-        }
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
-        }
-
-        @media (min-width: 769px) {
-          h1 {
-            font-size: 3rem;
-          }
-          h2 {
-            font-size: 2.25rem;
-          }
-        }
-      `}</style>
     </>
   );
 };

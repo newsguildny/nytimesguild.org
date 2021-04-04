a8b35aeacd5c80f4795c363e18a7866062b982cc
Joseph Kohlmann <kohlmannj@mac.com>
2021-04-03 20:19:03 -0400

    TSConfig: enable incremental compilation, add `clean` script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.(ts|tsx|json)': (filenames) => [`eslint --fix ${filenames.join(' ')}`, 'tsc --noEmit -p .'],
+  '*.(ts|tsx|json)': (filenames) => [`eslint --fix ${filenames.join(' ')}`, 'yarn type-check'],
   '*.(yml|json|html|css)': 'prettier --write',
 };

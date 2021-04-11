de8c3e228957cfa96c430ac585ccea935256b870
Joseph Kohlmann <kohlmannj@mac.com>
2021-04-11 18:22:52 -0400

    Add support for reordering pages in navigation menu

--- src/lib/collections/pages.ts
+++ src/lib/collections/pages.ts
@@ -11,6 +11,7 @@ interface PageData {
   title: string;
   heading?: string;
   subheading?: string;
+  navigationOrder: number;
 }
 
 export function getPageSlugs() {
@@ -43,5 +44,11 @@ export async function getPageData(filename: string, staticContext?: StaticContex
 
 export const getNavigationData = async (slug?: string) => ({
   activeSlug: slug ?? null,
-  pagesMetadata: getPagesMetadata().filter(({ slug: pageSlug }) => pageSlug !== 'index'),
+  pagesMetadata: getPagesMetadata()
+    .filter(({ slug: pageSlug }) => pageSlug !== 'index')
+    .sort((first, second) => {
+      if (first.navigationOrder < second.navigationOrder) return -1;
+      if (first.navigationOrder > second.navigationOrder) return 1;
+      return 0;
+    }),
 });

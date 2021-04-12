5bde5365964dddc528dbccb33b525a76317c0a1c
Joseph Kohlmann <kohlmannj@mac.com>
2021-04-12 19:44:34 -0400

    Add “Show in Navigation Menu” option to pages

--- src/lib/collections/pages.ts
+++ src/lib/collections/pages.ts
@@ -11,6 +11,7 @@ interface PageData {
   title: string;
   heading?: string;
   subheading?: string;
+  navigationShow: boolean;
   navigationOrder: number;
 }
 
@@ -45,7 +46,7 @@ export async function getPageData(filename: string, staticContext?: StaticContex
 export const getNavigationData = async (slug?: string) => ({
   activeSlug: slug ?? null,
   pagesMetadata: getPagesMetadata()
-    .filter(({ slug: pageSlug }) => pageSlug !== 'index')
+    .filter(({ navigationShow, slug: pageSlug }) => pageSlug !== 'index' && !!navigationShow)
     .sort((first, second) => {
       if (first.navigationOrder < second.navigationOrder) return -1;
       if (first.navigationOrder > second.navigationOrder) return 1;

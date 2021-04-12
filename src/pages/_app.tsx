a4683572a42158e053cbbdc5505d96f15e399cb9
Joseph Kohlmann (he/him/his) <kohlmannj@mac.com>
2021-04-11 21:20:01 -0400

    Add Site Favicons (#134)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Navigation } from '../components/Navigation';
 import { Footer } from '../components/Footer';
 import StaticContext, {
@@ -22,6 +23,15 @@ export default function AppWithContext({
 }: AppPropsWithContext) {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff4040" />
+        <meta name="msapplication-TileColor" content="#ff4040" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <StaticContext.Provider value={staticContext}>
         <Navigation />
         <Component {...pageProps} />

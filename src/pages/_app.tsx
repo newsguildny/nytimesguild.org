a15a3cccad2fdd58d6bc6039c93bdbe34e3dcc26
smoores <shane@shanemoore.me>
2021-04-13 15:33:38 +0000

    Make og:image tag absolute

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,7 +32,7 @@ export default function AppWithContext({
         <meta name="apple-mobile-web-app-title" content="NYT Guild" />
         <meta name="msapplication-TileColor" content="#ff4040" />
         <meta name="theme-color" content="#ffffff" />
-        <meta name="og:image" content="/og-image.png" />
+        <meta name="og:image" content="https://nytimesguild.org/og-image.png" />
       </Head>
       <StaticContext.Provider value={staticContext}>
         <Navigation />

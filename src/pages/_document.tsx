fe4d6a99faf620243b9dc61c7028ef19f4279024
SMores <5354254+SMores@users.noreply.github.com>
2021-04-14 23:51:46 -0400

    Add fathom analytics (#194)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,19 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script>{`
+            (function(f, a, t, h, o, m){
+              a[h]=a[h]||function(){
+                (a[h].q=a[h].q||[]).push(arguments)
+              };
+              o=f.createElement('script'),
+              m=f.getElementsByTagName('script')[0];
+              o.async=1; o.src=t; o.id='fathom-script';
+              m.parentNode.insertBefore(o,m)
+            })(document, window, '//fathom.shanemoore.me/tracker.js', 'fathom');
+            fathom('set', 'siteId', 'JJDAK');
+            fathom('trackPageview');
+          `}</script>
         </body>
       </Html>
     );

4b41406838649cca25de5d7b71cd11ed316aec73
Shane Moore <shanewmoore@gmail.com>
2021-04-15 04:07:37 +0000

    Use dangerouslySetInnerHTML for fathom script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,19 +16,22 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script>{`
-            (function(f, a, t, h, o, m){
-              a[h]=a[h]||function(){
-                (a[h].q=a[h].q||[]).push(arguments)
-              };
-              o=f.createElement('script'),
-              m=f.getElementsByTagName('script')[0];
-              o.async=1; o.src=t; o.id='fathom-script';
-              m.parentNode.insertBefore(o,m)
-            })(document, window, '//fathom.shanemoore.me/tracker.js', 'fathom');
-            fathom('set', 'siteId', 'JJDAK');
-            fathom('trackPageview');
-          `}</script>
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `(function(f, a, t, h, o, m){
+                a[h]=a[h]||function(){
+                  (a[h].q=a[h].q||[]).push(arguments)
+                };
+                o=f.createElement('script'),
+                m=f.getElementsByTagName('script')[0];
+                o.async=1; o.src=t; o.id='fathom-script';
+                m.parentNode.insertBefore(o,m)
+              })(document, window, '//fathom.shanemoore.me/tracker.js', 'fathom');
+              fathom('set', 'siteId', 'JJDAK');
+              fathom('trackPageview');`,
+            }}
+          />
         </body>
       </Html>
     );

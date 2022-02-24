4aebcf8d41c02a4c812d44983d3f608e604597e1
Jeff Sisson <jeff@bigboy.us>
2022-02-23 21:14:52 -0500

    fixing aria, halfway, table

--- src/pages/vote-count.tsx
+++ src/pages/vote-count.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import { initializeApp } from "firebase/app";
-import { GetStaticProps } from "next";
-import Head from "next/head";
-import { PageHeader } from "../components/PageHeader";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { PageHeader } from '../components/PageHeader';
 
 function format(n: number): string {
   return `${(n * 100).toFixed(2)}%`;
@@ -16,40 +16,40 @@ const VoteCounts = () => {
   const [total, setTotal] = useState<number>(0);
   useEffect(() => {
     const firebaseConfig = {
-      apiKey: "AIzaSyCURKa7asmTfECkxa0S83ew5gLirPBdiFc",
-      authDomain: "nyttechvotecount.firebaseapp.com",
-      databaseURL: "https://nyttechvotecount-default-rtdb.firebaseio.com",
-      projectId: "nyttechvotecount",
-      storageBucket: "nyttechvotecount.appspot.com",
-      messagingSenderId: "45909778564",
-      appId: "1:45909778564:web:02866d0927cfb9478f62d1",
+      apiKey: 'AIzaSyCURKa7asmTfECkxa0S83ew5gLirPBdiFc',
+      authDomain: 'nyttechvotecount.firebaseapp.com',
+      databaseURL: 'https://nyttechvotecount-default-rtdb.firebaseio.com',
+      projectId: 'nyttechvotecount',
+      storageBucket: 'nyttechvotecount.appspot.com',
+      messagingSenderId: '45909778564',
+      appId: '1:45909778564:web:02866d0927cfb9478f62d1',
     };
 
     const app = initializeApp(firebaseConfig);
 
     const db = getDatabase(app);
-    const yesRef = ref(db, "yes");
+    const yesRef = ref(db, 'yes');
     onValue(yesRef, (snapshot) => {
       const data = snapshot.val();
-      if (typeof data === "number") {
+      if (typeof data === 'number') {
         setYes(data);
       }
     });
-    onValue(ref(db, "no"), (snapshot) => {
+    onValue(ref(db, 'no'), (snapshot) => {
       const data = snapshot.val();
-      if (typeof data === "number") {
+      if (typeof data === 'number') {
         setNo(data);
       }
     });
-    onValue(ref(db, "total"), (snapshot) => {
+    onValue(ref(db, 'total'), (snapshot) => {
       const data = snapshot.val();
-      if (typeof data === "number") {
+      if (typeof data === 'number') {
         setTotal(data);
       }
     });
-    onValue(ref(db, "contested"), (snapshot) => {
+    onValue(ref(db, 'contested'), (snapshot) => {
       const data = snapshot.val();
-      if (typeof data === "number") {
+      if (typeof data === 'number') {
         setContested(data);
       }
     });
@@ -66,17 +66,17 @@ const VoteCounts = () => {
       <main>
         <h1 id="heading">Results</h1>
         <div className="bar">
-          <div className="bar-yes" style={{ width: `${yes / total}%` }}></div>
+          <div className="bar-yes" style={{ width: `${yes / total}%` }} />
           <div
             className="bar-half"
-            style={{ left: `${Math.floor(50 + total / 100)}%` }}
-          ></div>
-          <div className="bar-no" style={{ width: `${no / total}%` }}></div>
+            style={{ left: `${((Math.floor(total / 2) + 1) / total) * 100}%` }}
+          />
+          <div className="bar-no" style={{ width: `${no / total}%` }} />
         </div>
-        <table aria-labelledby="heading">
+        <table>
           <thead>
             <tr>
-              <th></th>
+              <th aria-label="Vote category" />
               <th>Votes</th>
               <th>Pct. of uncontested</th>
             </tr>
@@ -92,10 +92,22 @@ const VoteCounts = () => {
               <td>{no}</td>
               <td>{format(no / uncontested)}</td>
             </tr>
+          </tbody>
+        </table>
+        <h2>Ballot data</h2>
+        <table>
+          <tbody>
             <tr>
-              <td>Contested</td>
+              <td>Ballots received</td>
+              <td>{total}</td>
+            </tr>
+            <tr>
+              <td>Ballots contested</td>
               <td>{contested}</td>
-              <td>{format(contested / uncontested)}</td>
+            </tr>
+            <tr>
+              <td>Uncontested ballots</td>
+              <td>{total - contested}</td>
             </tr>
           </tbody>
         </table>
